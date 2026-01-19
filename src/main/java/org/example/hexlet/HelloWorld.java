3842c10b59b9d6322bb83068c5058852f87fff10
Grigoriy <kulikovGM@gmail.com>
2026-01-19 19:56:29 +0300

    Update project

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,7 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -8,12 +9,23 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         app.get("/hello", ctx -> {
-	    var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World!");
- 	    ctx.contentType("text/html");
-   	    ctx.result("Hello, " + name + "!");
-	});
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.contentType("text/html");
+            ctx.result("Hello, " + name + "!");
+        });
         app.get("/users", ctx -> ctx.result("GET /users"));
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
         app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/users/{usersId}/post/{postId}", ctx -> {
+            var usersId = ctx.pathParam("usersId");
+            var postId = ctx.pathParam("postId");
+            ctx.result("Course ID: " + usersId + " Lesson ID: " + postId);
+        });
         app.start(7070);
     }
 }
